13baa613fa8dbf3f662f50ddced3b38a34e2234b
whamondg <gordon.whamond@googlemail.com>
2016-06-06 12:49:40 +0100

    log github event processing

--- test/controllers/githook_controller_test.exs
+++ test/controllers/githook_controller_test.exs
@@ -3,23 +3,78 @@ defmodule Githooker.GithookControllerTest do
   import ExUnit.CaptureLog
 
   test "Github deploy events are handled", %{conn: conn} do
-    test_github_event = "deployment"
+    test_event = "deployment"
 
     result = conn
-    |> put_req_header("x-github-event", test_github_event)
+    |> put_req_header("x-github-event", test_event)
     |> post("/api/githook")
 
-    assert json_response(result, 200) == %{"status" => "OK", "event" => test_github_event} 
+    assert json_response(result, 200) == %{"status" => "OK", "event" => test_event} 
   end
 
   test "Unknown Github events are not handled", %{conn: conn} do
-    test_github_event = "some unhandled event"
+    test_event = "some_unhandled_event"
 
     result = conn
-    |> put_req_header("x-github-event", test_github_event)
+    |> put_req_header("x-github-event", test_event)
     |> post("/api/githook")
 
-    assert json_response(result, 400) == %{"status" => "Unknown event", "event" => test_github_event}
+    assert json_response(result, 400) == %{"status" => "Unknown event", "event" => test_event}
+  end
+
+  test "All received GitHub events are logged", %{conn: conn} do
+    Logger.configure([level: :info])
+
+    test_events = ~w(
+      commit_comment create delete deployment deployment_status fork gollum 
+      issue_comment issues member membership page_build public pull_request 
+      pull_request_review_comment push release repository status team_add watch 
+      some_unknown_event
+    )
+
+    Enum.map(test_events, fn (test_event) ->
+      logs = capture_log([level: :info],fn ->
+        conn
+        |> put_req_header("x-github-event", test_event)
+        |> post("/api/githook")
+      end)
+
+      assert logs =~ "Received GitHub event: #{test_event}"
+    end)
+
+    Logger.configure([level: :warn])
+  end
+
+  test "Deployment events are logged", %{conn: conn} do
+    Logger.configure([level: :info])
+
+    test_event = "deployment"
+
+    logs = capture_log([level: :info],fn ->
+      conn
+      |> put_req_header("x-github-event", test_event)
+      |> post("/api/githook")
+    end)
+
+    assert logs =~ "Handling deployment event"
+
+    Logger.configure([level: :warn])
+  end
+
+  test "Unknown events are logged", %{conn: conn} do
+    Logger.configure([level: :info])
+
+    test_event = "some unknown event"
+
+    logs = capture_log([level: :info],fn ->
+      conn
+      |> put_req_header("x-github-event", test_event)
+      |> post("/api/githook")
+    end)
+
+    assert logs =~ "Ignoring unknown event: #{test_event}"
+
+    Logger.configure([level: :warn])
   end
 
 end
--- web/controllers/githook_controller.ex
+++ web/controllers/githook_controller.ex
@@ -1,17 +1,21 @@
 defmodule Githooker.GithookController do
   use Githooker.Web, :controller
   alias Githooker.EventHandler
+  require Logger
 
   def webhook(conn, _params) do
     github_event = List.first(get_req_header(conn, "x-github-event"))
+    Logger.info "Received GitHub event: #{github_event}"
     handleEvent(conn, %{event: github_event})
   end
 
   def handleEvent(conn, %{event: "deployment"}) do
+    Logger.info "Handling deployment event"
     json conn, %{ status: "OK", event: "deployment" }
   end
 
   def handleEvent(conn, %{event: event}) do
+    Logger.info "Ignoring unknown event: #{event}"
     json put_status(conn, 400), %{ status: "Unknown event", event: event }
   end
 end
