930c64bc0ef46ed2765dc8f5f676c857293f125b
whamondg <gordon.whamond@googlemail.com>
2016-06-06 11:12:54 +0100

    remove the case statement from githook_controller

--- web/controllers/githook_controller.ex
+++ web/controllers/githook_controller.ex
@@ -4,14 +4,15 @@ defmodule Githooker.GithookController do
 
   def webhook(conn, _params) do
     github_event = List.first(get_req_header(conn, "x-github-event"))
+    handleEvent(conn, %{event: github_event})
+  end
 
-    case {github_event} do
-       {"deployment"} ->
-         json conn, %{ status: "OK", event: github_event } 
-       _ ->
-         json put_status(conn, 400), %{ status: "Unknown event", event: github_event }
-    end
+  def handleEvent(conn, %{event: "deployment"}) do
+    json conn, %{ status: "OK", event: "deployment" }
+  end
+
+  def handleEvent(conn, %{event: event}) do
+    json put_status(conn, 400), %{ status: "Unknown event", event: event }
   end
 end
-
 
